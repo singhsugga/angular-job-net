41068216703912e53b0e9edcd0e57519717581dc
singhsugga <singhsugga@outlook.com>
2024-01-21 16:34:16 +0530

    feat: Added `Default Page Component`

--- apps/job-net-host/src/app/app.component.ts
+++ apps/job-net-host/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DefaultPageComponent } from './default-page/default-page.component';
 
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, DefaultPageComponent],
   selector: 'job-net-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ apps/job-net-host/src/app/default-page/default-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'job-net-default-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './default-page.component.html',
+  styleUrl: './default-page.component.scss',
+})
+export class DefaultPageComponent {}
