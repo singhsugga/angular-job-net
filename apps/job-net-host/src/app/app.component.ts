8764ac1f7c35c27350ba399359ea3a9db2deddb4
singhsugga <singhsugga@outlook.com>
2024-01-21 16:04:18 +0530

    feat: changed prefix for component selectors from `angular-job-net` to `job-net`

--- apps/job-net-host/src/app/app.component.ts
+++ apps/job-net-host/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 @Component({
   standalone: true,
   imports: [NxWelcomeComponent, RouterModule],
-  selector: 'angular-job-net-root',
+  selector: 'job-net-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
